a725541904d7c457d3206f55c8b34ce91c73a3b4
sungmin kim <ignocide@gmail.com>
2019-11-05 12:11:05 +0900

    project init

---
+++ src/telegram/health.ts
@@ -0,0 +1,12 @@
+import { Message, OnText, TelegramRouter, Bot, OnRoute } from "../lib/telegram";
+
+class HealthBot extends TelegramRouter {
+
+  @OnRoute('ping')
+  ping(msg: Message, bot: Bot) {
+    const chatId = msg.chat.id
+    bot.sendMessage(chatId, 'pong')
+  }
+}
+
+export default new HealthBot()
